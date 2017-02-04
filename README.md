ce94bf3888e38d0b458c5f389024f3dc4aadf9bf
lost-RD <lost_RD@live.com>
2017-02-04 16:52:00 +1030

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Wild Animal Alert
+RimWorld mod for Spartyon7
+
+Alerts the player to all wild animals that wander onto the map.
+
+##Changelog:
+
+###1.0:
+* threw it together in an hour
+* things seem to work
---
+++ Source/WildAnimalAlert/WildSpawner.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace RD_WildAnimalAlert
+{
+	public class _WildSpawner
+	{
+		private const int AnimalCheckInterval = 1210;
+
+		private const float BaseAnimalSpawnChancePerInterval = 0.0268888883f;
+
+		private const int PlantTrySpawnIntervalAt100EdgeLength = 650;
+
+		private Map map;
+
+		public _WildSpawner(Map map)
+		{
+			this.map = map;
+		}
+
+		public void _SpawnRandomWildAnimalAt(IntVec3 loc)
+		{
+			PawnKindDef pawnKindDef = (from a in this.map.Biome.AllWildAnimals
+			where this.map.mapTemperature.SeasonAcceptableFor(a.race)
+			select a).RandomElementByWeight((PawnKindDef def) => this.map.Biome.CommonalityOfAnimal(def) / def.wildSpawn_GroupSizeRange.Average);
+			if (pawnKindDef == null)
+			{
+				Log.Error("No spawnable animals right now.");
+				return;
+			}
+			int randomInRange = pawnKindDef.wildSpawn_GroupSizeRange.RandomInRange;
+			int radius = Mathf.CeilToInt(Mathf.Sqrt((float)pawnKindDef.wildSpawn_GroupSizeRange.max));
+			for (int i = 0; i < randomInRange; i++)
+			{
+				IntVec3 loc2 = CellFinder.RandomClosewalkCellNear(loc, this.map, radius);
+				Pawn newThing = PawnGenerator.GeneratePawn(pawnKindDef, null);
+				GenSpawn.Spawn(newThing, loc2, this.map);
+				Find.LetterStack.ReceiveLetter("A wild "+newThing.Label+" appeared!", newThing.gender.ToString()+" "+newThing.Label+"\n\n"+newThing.ageTracker.AgeBiologicalYears+" years old\n\nIf you no longer wish to see these messages, disable RD_WildAnimalAlert in the Mods menu.", LetterType.Good, new TargetInfo(loc2, map, false), null);
+			}
+		}
+	}
+}
